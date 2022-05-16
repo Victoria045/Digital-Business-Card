1d58f1cf330c49f23fe8f6c53812663c3cbd01ff
Victoria045 <victoriaberyl45@gmail.com>
2022-05-16 21:50:48 +0300

    modify the landing page

--- digital-business-card/src/components/Footer.js
+++ digital-business-card/src/components/Footer.js
@@ -2,10 +2,10 @@ export default function Footer() {
   return (
     <div className="=footer-wrap">
       <div className="footer">
-        <i class="fa fa-twitter-square fa-2x" aria-hidden="true"></i>
-        <i class="fa fa-facebook-square fa-2x" aria-hidden="true"></i>
-        <i class="fa fa-instagram fa-2x" aria-hidden="true"></i>
-        <i class="fa fa-github-square fa-2x" aria-hidden="true"></i>
+        <i className="fa fa-twitter-square fa-2x" aria-hidden="true"></i>
+        <i className="fa fa-facebook-square fa-2x" aria-hidden="true"></i>
+        <i className="fa fa-instagram fa-2x" aria-hidden="true"></i>
+        <i className="fa fa-github-square fa-2x" aria-hidden="true"></i>
       </div>
     </div>
     
